75237095432a682558eaeb46166e382641c68f04
Jose Lemuz <lemuz.jose@gmail.com>
2022-01-20 15:55:59 -0800

    final version

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ const generateChar = (min, max)=>{
     return String.fromCharCode(randomChar);
 } 
 //Function to select random value from function array
-//Will be used to randonly select a function based on the filtered array
+//Will be used to randomly select a function based on the filtered array
 const generateNum = (min, max)=>{
   randomFunc = Math.floor(Math.random() * (max - min) + min);
   return randomFunc;
@@ -19,7 +19,7 @@ const generateUpper = ()=> generateChar(65,90);
 const generateLower= ()=> generateChar(97, 122);
 const generateSpecial= ()=> generateChar(33, 47);
 const generateSpecial2= ()=> generateChar(58,64);
-//returns a number not a string
+//returns a number not a string 
 const generateNumber = ()=> generateNum(0,9);
 
 //This is a set of prompts for the length and all the criteria options
@@ -46,13 +46,14 @@ const specialPrompt = ()=> {
 }
 //This function takes in the prompt parameters, passes the values into the function array
 //and generates the random password
-
 const generatePassword = ()=>{
   //This prompt will occur until the user enters a digit betweem 8-128
-  do {length = lengthPrompt();}
-  while(( /^[0-9.,]+$/.test(length))===false || ((/^[8-9]$|^[1-9][0-9]$|^[1][012][0-8]$/).test(length))===false)
+  
+    do {length = lengthPrompt();}
+    while(( /^[0-9.,]+$/.test(length))===false || ((/^[8-9]$|^[1-9][0-9]$|[1][0][9]|^[1][012][0-8]$/).test(length))===false)
 
   //This set of confirm prompts will appear until at least one criteria is selected
+  //Then this set of values are assigned to a variable which is passed to the function list array
   do{
     window.alert("You must confirm at least one of the criteria")
     upper = upperPrompt();
@@ -64,7 +65,8 @@ const generatePassword = ()=>{
     //this while case shows that all the criteria were set to false
   while(upper!=true && lower!=true && special!=true && num!=true)
 
-//Array of functions initialized
+//Array of functions initialized and the 'include' key value pair is whether the user chose to include the character in the initial prompts
+//Include value is a boolean value
   const funcList = [
     {name: generateUpper, include: upper},  //0
     {name: generateLower, include: lower},   //1
@@ -73,7 +75,7 @@ const generatePassword = ()=>{
     {name: generateNumber, include: num}    //4
   ];
 
-  //This function filters out the false crieria and returns an array of functions with only the
+  //This function filters out the false criteria and returns an array of functions with only the
   //included criteria
   const passPattern = funcList.filter(a=> a.include===true).map(i=> {return i.name;});
   //The password string is initialized and the for loop loops through the filtered 
@@ -95,5 +97,6 @@ function writePassword() {
   passwordText.value = password;
 
 }
-// // // Add event listener to generate button
+
+// Add event listener to generate button
  generateBtn.addEventListener("click", writePassword);
